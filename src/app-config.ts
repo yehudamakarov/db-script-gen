375ca72571d829f503a2c4cbd432c1ea50aad062
Yehuda Makarov <yehudamakarov@gmail.com>
2021-07-30 01:40:03 -0400

    mvp finished

--- src/app-config.ts
+++ src/app-config.ts
@@ -2,19 +2,25 @@ import * as fs from 'fs'
 import {Command} from '@oclif/command'
 import {MyConfig} from './commands/gen'
 import * as path from 'path'
+import * as os from 'os'
 
-function validateConf(conf: MyConfig) {
+function validateConf(conf: MyConfig, confLoc: string) {
+  const plsCheck = os.EOL + 'please check out ' + confLoc
   if (!conf.scriptsLocation) {
-    throw new Error('There was no config found for |scriptsLocation| - please set it! Unless things are going nice without the config... in which case, we are all good here! :) Have a great day!')
+    throw new Error('There was no config found for |scriptsLocation| - please set it! Unless things are going nice without the config... in which case, we are all good here! :) Have a great day! ' + plsCheck)
+  }
+  if (!fs.existsSync(conf.scriptsLocation)) {
+    throw new Error('The scriptsLocation config setting fails a check with fs.existsSync(). Please verify the location on disc. ' + plsCheck)
   }
 }
 
 export const appConfig = (commandClass: Command) => {
+  let confLoc = path.join(commandClass.config.configDir, 'config.json')
   const conf = JSON.parse(
     fs
-      .readFileSync(path.join(commandClass.config.configDir, 'config.json'))
+      .readFileSync(confLoc)
       .toString(),
   ) as MyConfig
-  validateConf(conf)
+  validateConf(conf, confLoc)
   return conf
 }
--- src/commands/gen.ts
+++ src/commands/gen.ts
@@ -1,37 +1,54 @@
-import {Command, flags} from '@oclif/command'
+import {Command} from '@oclif/command'
 import {appConfig} from '../app-config'
 import * as inquirer from 'inquirer'
+import * as path from 'path'
+import * as fs from 'fs'
+import cli from 'cli-ux'
+import * as os from 'os'
+import {getRandomMessage, getRandomNumber} from '../messages'
+import chalk from 'chalk'
 
 export interface MyConfig {
   scriptsLocation: string
 }
 
+export interface Answers {
+  ticket: string,
+  description: string,
+  typeOfScript: string,
+  confirm: boolean
+}
+
+const sleep = async (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
 export default class Gen extends Command {
   static description = 'generate a file that will contain scripts for a target database'
 
-  static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
-
   async run() {
+    cli.action.start('checking config')
+    await sleep(1000)
     const ac = appConfig(this)
+    cli.action.stop()
 
-    const responses = await inquirer.prompt([{
+    const responses = await inquirer.prompt<Answers>([{
       name: 'ticket',
       message: 'What ticket are you working on?',
       type: 'input',
     }, {
-      name: 'type of script',
+      name: 'typeOfScript',
       message: 'What type of script do you need to create?',
       type: 'list',
       choices: [
-        {name: 'add a table'}, {name: 'alter a table'}, {name: 'misc'}],
+        {
+          name: 'add a table',
+          value: 'add.table',
+        }, {
+          name: 'alter a table',
+          value: 'alter.table',
+        }, {
+          name: 'misc',
+          value: 'misc',
+        }],
     }, {
       name: 'description',
       message: 'Additional description?',
@@ -42,7 +59,39 @@ export default class Gen extends Command {
       type: 'confirm',
     }])
 
-    console.log(ac.scriptsLocation)
-    console.table(responses)
+    cli.action.start(getRandomMessage())
+    const fn = Gen.getFileName(responses)
+    fs.writeFileSync(path.join(ac.scriptsLocation, fn), '-- ' + responses.ticket + os.EOL)
+    await sleep(getRandomNumber(5, 2) * 1000)
+    cli.action.stop('phew.')
+    cli.log(chalk.greenBright('Done! created: ' + fn))
+  }
+
+  private static getFileName(responses: any) {
+    const dateString = Gen.getTimestamp()
+    let fn = 'V' + dateString + '__'
+    if (responses.ticket.indexOf('-') == -1) {
+      cli.warn('Make sure your ticket name is correct, it didn\'t have a hyphen in it. They usually do.')
+    }
+    fn += responses.ticket ? responses.ticket : 'no-ticket'
+
+    fn += '---'
+    fn += responses.typeOfScript
+    if (responses.description) {
+      fn += '---'
+      fn += responses.description.replace(/\s+/g, '-').toLowerCase()
+    }
+    fn += '.sql'
+    return fn
+  }
+
+  private static getTimestamp() {
+    const date = new Date()
+    return date.getFullYear() + '.'
+      + ('00' + (date.getMonth() + 1)).slice(-2) + '.'
+      + ('00' + date.getDate()).slice(-2) + '.'
+      + ('00' + date.getHours()).slice(-2) + '.'
+      + ('00' + date.getMinutes()).slice(-2) + '.'
+      + ('00' + date.getSeconds()).slice(-2)
   }
 }
