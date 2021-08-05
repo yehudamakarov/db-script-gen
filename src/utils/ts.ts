53c7f48b0ee2f095455ece69c334e4eedeafc652
Yehuda Makarov <ymakarov@fabuwood.com>
2021-08-05 15:09:11 -0400

    digits for year needs to be 4 long closes #5

--- src/utils/ts.ts
+++ src/utils/ts.ts
@@ -1,7 +1,9 @@
 export const getTimestamp = () => {
-  const pad = (unit: number) => (('00' + unit).slice(-2))
-  const date = new Date()
-  return [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()]
-    .map(pad)
-    .join('.')
+	const pad = (unit: number) => (('00' + unit).slice(-2))
+	const date = new Date()
+	return [date.getFullYear().toString()]
+		.concat(
+			[date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()].map(pad)
+		)
+		.join('.')
 }
